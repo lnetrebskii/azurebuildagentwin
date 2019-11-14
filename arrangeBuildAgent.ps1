bb005bc2d1e0884cd50885cb5875db922c44dedc
Leo <leon@entechsolutions.com>
2019-11-14 20:43:48 +0400

    Fix Write-Verbose

--- arrangeBuildAgent.ps1
+++ arrangeBuildAgent.ps1
@@ -20,39 +20,39 @@ Function Write-Verbose {
 }
 
 trap {
-    WriteLog ("Error: {0}" -f $Error[0].Exception)
+    Write-Verbose ("Error: {0}" -f $Error[0].Exception)
 }
 
-WriteLog("start script")
-WriteLog("vstsURL: $vsts_URL")
-WriteLog("agentPool: $agent_Pool")
-WriteLog("agentName: $agent_Name")
-WriteLog("agenttag: $agent_Tag")
+Write-Verbose("start script")
+Write-Verbose("vstsURL: $vsts_URL")
+Write-Verbose("agentPool: $agent_Pool")
+Write-Verbose("agentName: $agent_Name")
+Write-Verbose("agenttag: $agent_Tag")
 
 
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 
 #Install dotnet core, nodejs and npm with chocolatey
-WriteLog("install chocholatey")
+Write-Verbose("install chocholatey")
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
 choco feature enable -n allowGlobalConfirmation;
 
-WriteLog("install dotnetcore 2.2")
+Write-Verbose("install dotnetcore 2.2")
 choco install dotnetcore-sdk --version=2.2 -y;
 
-WriteLog("install dotnetcore the latest")
+Write-Verbose("install dotnetcore the latest")
 choco install dotnetcore-sdk -y;
 
-WriteLog("install nodejs")
+Write-Verbose("install nodejs")
 choco install nodejs -y;
 
-WriteLog("install webdeploy")
+Write-Verbose("install webdeploy")
 choco install webdeploy -y;
 
-WriteLog("install java")
+Write-Verbose("install java")
 choco install webdeploy -y;
 
-WriteLog("download buildagent")
+Write-Verbose("download buildagent")
 
 $downloadDirectory = Join-Path $env:SystemDrive 'agent'
 New-Item -Path $downloadDirectory -ItemType Directory -force | Out-Null
@@ -62,15 +62,15 @@ $url = "https://vstsagentpackage.azureedge.net/agent/2.146.0/vsts-agent-win-x64-
 $file = Join-Path $downloadDirectory ([System.IO.Path]::GetFileName($url))
 $clnt.DownloadFile($url, $file)
 
-WriteLog("expand build agent")
+Write-Verbose("expand build agent")
 Expand-Archive -Path $file -DestinationPath $downloadDirectory
 
-WriteLog("install build agent")
+Write-Verbose("install build agent")
 # Replaces agent with the same name in the same pool
 # Agent starts with Windows Login
 # Agent works as a service
 $agentoutput = C:/agent/bin/Agent.Listener.exe configure --url $vsts_URL --agent $agent_Name --pool $agent_Pool  --unattended --auth pat --token $pat_Token  --windowsLogonAccount "NT AUTHORITY\SYSTEM" --replace --runAsService --runAsAutoLogon
 
-WriteLog($agentoutput)
+Write-Verbose($agentoutput)
 
 Restart-Computer
