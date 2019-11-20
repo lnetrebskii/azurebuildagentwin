cb36a05317040e898caa67a9c0147c4cd73032e6
Leo <leon@entechsolutions.com>
2019-11-20 13:07:05 +0400

    Start using chocolatey to install build agent

--- arrangeBuildAgent.ps1
+++ arrangeBuildAgent.ps1
@@ -52,23 +52,5 @@ choco install webdeploy -y;
 Write-Verbose("install java")
 choco install webdeploy -y;
 
-Write-Verbose("download buildagent")
-
-$downloadDirectory = Join-Path $env:SystemDrive 'agent'
-New-Item -Path $downloadDirectory -ItemType Directory -force | Out-Null
-
-$clnt = new-object System.Net.WebClient
-$url = "https://vstsagentpackage.azureedge.net/agent/2.146.0/vsts-agent-win-x64-2.146.0.zip"
-$file = Join-Path $downloadDirectory ([System.IO.Path]::GetFileName($url))
-$clnt.DownloadFile($url, $file)
-
-Write-Verbose("expand build agent")
-Expand-Archive -Path $file -DestinationPath $downloadDirectory
-
 Write-Verbose("install build agent")
-# Replaces agent with the same name in the same pool
-# Agent starts with Windows Login
-# Agent works as a service
-$agentoutput = C:/agent/bin/Agent.Listener.exe configure --url $vsts_URL --agent $agent_Name --pool $agent_Pool  --unattended --auth pat --token $pat_Token  --windowsLogonAccount "NT AUTHORITY\SYSTEM" --replace --runAsService --runAsAutoLogon
-
-Write-Verbose($agentoutput)
+choco install azure-pipelines-agent --params "'/AgentName:$agent_Name /Directory:c:\agent /Url:$vsts_URL /Token:$pat_Token /Pool:$agent_Pool /LogonAccount:'NT AUTHORITY\SYSTEM' /Replace /AutoLogon'"
